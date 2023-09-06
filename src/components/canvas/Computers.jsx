e85afe444d5f0c4de1010d57771fa75effde4c5b
Paul Chow <cpaulwenkai@gmail.com>
2023-09-06 09:09:46 +0200

    Computers: add autoRotate

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -53,7 +53,6 @@ const ComputersCanvas = () => {
 
   }, [])
 
-  console.log("isMobile => ", isMobile)
   return (
     <Canvas
       frameloop="demand"
@@ -63,6 +62,7 @@ const ComputersCanvas = () => {
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
+          autoRotate
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2} />
