47a8751a9af1f9e926463aec880b85e8b41b686c
Paul Chow <cpaulwenkai@gmail.com>
2023-09-06 09:11:16 +0200

    About: add justify-center to style

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -42,7 +42,7 @@ const About = () => {
         I'm a skilled software developer with experience in Typescript and Javascript, and expertise in frameworks like React, Node.js and Three.js. I'm a quick learner and collaborate closely with clients to create efficient, scalable and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
 
-      <div className="mt-20 flex flex-wrap gap-10">
+      <div className="mt-20 flex flex-wrap justify-center gap-10">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
