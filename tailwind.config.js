1fdcfcf8092f4eeccf731d31d8969e7a0f4a88bd
Paul Chow <cpaulwenkai@gmail.com>
2023-09-07 17:48:22 +0200

    tailwind: update color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#050816",
-        secondary: "#aaa6c3",
-        tertiary: "#151030",
+        // primary: "#050816",
+        // secondary: "#aaa6c3",
+        // tertiary: "#151030",
+        primary: "#1b1a1a",
+        secondary: "#31b1ff",
+        tertiary: "#1c1435",
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
