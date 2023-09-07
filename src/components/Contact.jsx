c4949c44d266313f48490b919148c07f39ed624c
Paul Chow <cpaulwenkai@gmail.com>
2023-09-07 18:56:14 +0200

    Contact: update form placeholder

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -101,7 +101,7 @@ const Contact = () => {
               name="message"
               value={form.message}
               onChange={handleChange}
-              placeholder="What do you want to say?"
+              placeholder="What would you like to say?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
             />
           </label>
