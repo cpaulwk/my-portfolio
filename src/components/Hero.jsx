50a5c47820c2ad27d698ecb7b99155425cc3ddf5
Paul Chow <cpaulwenkai@gmail.com>
2023-10-03 17:29:28 +0200

    Hero: update style by adding a space

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,15 +7,22 @@ import { ComputersCanvas } from "./canvas";
 const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
-      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-secondary" />
           <div className="w-1 sm:h-80 h-40 blue-gradient" />
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-secondary">Paul</span></h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I build web applications<br className="sm:block hidden" />leveraging AI, 3D models...</p>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-secondary">Paul</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I build web applications
+            <br className="sm:block hidden" /> leveraging AI, 3D models...
+          </p>
         </div>
       </div>
       {/* <ComputersCanvas /> */}
@@ -25,7 +32,7 @@ const Hero = () => {
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
               animate={{
-                y: [0, 24, 0]
+                y: [0, 24, 0],
               }}
               transition={{
                 duration: 1.5,
@@ -37,8 +44,8 @@ const Hero = () => {
           </div>
         </a>
       </div>
-    </section >
-  )
-}
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
