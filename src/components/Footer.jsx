878fa79db48aa625896b69467c56423f2333148b
Paul Chow <cpaulwenkai@gmail.com>
2023-10-04 17:37:01 +0200

    Footer: create Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="w-screen flex flex-col items-center">
+      <p>
+        ⚠️ This is portfolio template is a complete reproduction of Adrian
+        Hajdin's and is still in maintenance
+      </p>
+      <p>
+        A complete new porfolio will be launched soon! Can't wait to meet you
+        there 🚀
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
